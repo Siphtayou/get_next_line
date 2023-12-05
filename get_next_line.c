913f68fc15ee1b33bdf8a140b622fc40ff40c2b7
Alex Gilles <agilles@c2r2p2.42lehavre.fr>
2023-12-05 18:37:18 +0100

    updt

--- get_next_line.c
+++ get_next_line.c
@@ -6,40 +6,62 @@
 /*   By: agilles <agilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 09:15:35 by agilles           #+#    #+#             */
-/*   Updated: 2023/11/29 16:19:03 by agilles          ###   ########.fr       */
+/*   Updated: 2023/12/05 17:24:44 by agilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	ft_newline(char *buff, char *stock)
+void	ft_newline(char *buff)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	i = 0;
+	while (buff[i] != '\n')
+		i++;
+	i++;
+	while (buff[j + i] != '\0')
+	{
+		buff[j] = buff[i + j];
+		j++;
+	}
+	buff[j] = '\0';
+}
+int		is_line(char *buff)
 {
 	int	i;
 
 	i = 0;
-	while (buff[i] != '\n' || buff[i] != '\0')
+	while (buff[i] != '\0')
 	{
-		
+		if (buff[i] == '\n')
+			return (1);
+		i++;
 	}
+	return(0);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	buff[BUFFER_SIZE + 1];
+	static char	buff[BUFFER_SIZE + 1] = "";
 	char		*stock;
 	int			byte;
 
 	stock = NULL;
-	buff[BUFFER_SIZE + 1] = "";
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (buff)
-		ft_newline(buff, stock); // cut tout jusqua \n et stock le reste dans stock
+		ft_newline(buff); // cut tout jusqua \n et stock le reste dans stock
 	while (!is_line(buff)) // is_line verifie si ya \n
 	{
 		byte = read(fd, buff, BUFFER_SIZE);
-		if (byte == BUFFER_SIZE || byte == 0)
-			buff[byte] = '\0';
+		if (byte == 0)
+			return (stock);
+		else if (byte == -1)
+			return (free(stock), 0);
+		buff[byte] = '\0';
 		ft_line(buff, stock); // ft_line deplace buff dans stock mais sarrete apres \n ou \0 si ya dans buff
 	}
 	return(stock);
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: agilles <agilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 09:22:13 by agilles           #+#    #+#             */
-/*   Updated: 2023/11/29 14:59:41 by agilles          ###   ########.fr       */
+/*   Updated: 2023/12/05 15:41:20 by agilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,9 @@
 # endif
 
 char	*get_next_line(int fd);
+void	ft_newline(char *buff);
+int		is_line(char *buff);
 
-int	ft_strlen(char *str);
-
+int		ft_strlen(char *str);
 
 #endif
